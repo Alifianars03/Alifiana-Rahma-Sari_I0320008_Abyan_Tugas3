1dab67d343cdf510bebdc5b6c0337dcf23dd55cf
Alifiana Rahma <alifianars03@gmail.com>
2021-03-19 17:27:01 +0700

    a

---
+++ I0320008_Soal1_Tugas3.py
@@ -0,0 +1,15 @@
+
+teman = ['Bagus','Dhea','Divana','Ara','Attar','Danen','Bernat','Memed','Hani','Nadiya']
+
+print("List indeks nomor 4,6, dan 7 adalah",teman[4],",",teman[6],", dan",teman[7])
+
+teman[3] = 'Bernat'
+teman[5] = 'Nirmala'
+teman[9] = 'Tiara'
+
+teman.append('Ahmad')
+teman.append('Shabrial')
+
+print(teman*2)
+
+print(len(teman))
---
+++ I0320008_Soal2_Tugas3.py
@@ -0,0 +1,11 @@
+# Membuat dictionary
+dict = {'Nama':'Alifiana','Hobi1' :'Memasak','Hobi3':'Membaca buku','Sosmed1':'ig:@Alifianarhm_','Sosmed2':'line:sarialifiana','Sosmed3':'twitter:@aliipppritty','Lagu1':'Location Unknown','Lagu2':'Melukis senja','Lagu3':'Monolog','Makanan1':'Magelangan','Makanan2':'Lasagna','Makanan3':'Dissum'}
+
+# Mengubah salah satu hobi dan sosmed, hapus 2 makanan, dan tambah 1 hobi
+dict['Hobi2'] = 'Kulineran'
+dict['Sosmed3'] = 'ig:aliiippppoo'
+del dict['Makanan3']
+del dict['Makanan2']
+dict['Hobi4'] = 'Bersosialisasi'
+
+print(dict)
